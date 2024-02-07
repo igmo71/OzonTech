6640677659cc3f29c4e70df843e59423e5f75527
Igor Mogilnitskiy <igmo71@outlook.com>
2024-02-08 00:17:02 +0300

    T09---

--- OzonTech/Program.cs
+++ OzonTech/Program.cs
@@ -1,39 +1,43 @@
-﻿namespace OzonTech
+﻿using System.Diagnostics;
+
+namespace OzonTech
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            using var input = new StreamReader(Console.OpenStandardInput());
-            using var output = new StreamWriter(Console.OpenStandardOutput());
-
-            int fieldsCount = int.Parse(input.ReadLine()!);
+            //Debug.WriteLine($"=====> Start Application");
+            using StreamReader input = new StreamReader(Console.OpenStandardInput(), bufferSize: 16384);
+            using StreamWriter output = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 16384);
+            string? fieldsCountString = input.ReadLine();
+            int fieldsCount = int.Parse(fieldsCountString!);
             Field[] fields = new Field[fieldsCount];
+
+            //var iputStart = Stopwatch.GetTimestamp();
 
             for (int i = 0; i < fieldsCount; i++)
             {
                 string[] size = input.ReadLine()!.Split(' ');
                 fields[i].rowCount = int.Parse(size[0]);
                 fields[i].colCount = int.Parse(size[1]);
-                fields[i].rows = new string[int.Parse(size[0])];
+                fields[i].rows = new string[fields[i].rowCount];
                 for (int j = 0; j < fields[i].rowCount; j++)
                 {
                     fields[i].rows[j] = input.ReadLine()!;
+                    //Debug.WriteLine($"fields[{i}].rows[{j}]");
                 }
             }
+            //output.WriteLine($"input - {Stopwatch.GetElapsedTime(iputStart).TotalMilliseconds}");
 
+            Console.Write("\r");
+            var processStart = Stopwatch.GetTimestamp();
             foreach (Field field in fields)
             {
                 string result = field.Process();
                 output.WriteLine(result);
             }
+            //output.WriteLine($"process - {Stopwatch.GetElapsedTime(processStart).TotalMilliseconds}");
         }
-    }
-
-    struct InputFieldData
-    {
-        public string size;
-        public string[] rows;
     }
 
     struct Field
@@ -44,80 +48,70 @@
 
         public string Process()
         {
-            string result = string.Empty;
-            List<int> results = new();
+            List<int> internalCountList = new();
 
-            char[,] rectangle = ParseRectangle();
+            //var parseSrart = Stopwatch.GetTimestamp();
+            var frames = ParseFrames();
+            //Console.WriteLine($"parse - {Stopwatch.GetElapsedTime(parseSrart)}");
 
-            var frames = ParseFrames(rectangle);
+            //var processStart =  Stopwatch.GetTimestamp();
             for (int i = 0; i < frames.Count; i++)
             {
                 int internalCount = 0;
                 for (int j = 0; j < frames.Count; j++)
                 {
-                    if (Frame.InternalTo(frames[i], frames[j]))
+                    if (i != j && Frame.InternalTo(frames[i], frames[j]))
                     {
                         internalCount++;
+                        //Debug.WriteLine($"internalCount => {internalCount}");
                     }
                 }
-                results.Add(internalCount);
+                internalCountList.Add(internalCount);
             }
-            results.Sort();
-            foreach (var r in results)
-                result = $"{result}{r} ";
+            internalCountList.Sort();
+
+            string result = string.Join(' ', internalCountList);
+            //Console.WriteLine($"process - {Stopwatch.GetElapsedTime(processStart)}"); 
             return result;
         }
 
-        private char[,] ParseRectangle()
-        {
-            char[,] rectangle = new char[rowCount, colCount];
-            for (int i = 0; i < rows.Length; i++)
-            {
-                for (int j = 0; j < rows[i].Length; j++)
-                {
-                    rectangle[i, j] = rows[i][j];
-                }
-            }
-            return rectangle;
-        }
-
-        private List<Frame> ParseFrames(char[,] rectangle)
+        private List<Frame> ParseFrames()
         {
             List<Frame> frames = new();
             for (int i = 0; i < rowCount; i++)
             {
                 for (int j = 0; j < colCount; j++)
                 {
-                    if (rectangle[i, j] == '*')
+                    if (rows[i][j] == '*')
                     {
-                        if (i < rowCount - 1 && j < colCount - 1 && rectangle[i, j + 1] == '*' && rectangle[i + 1, j] == '*') // topLeft
+                        if (i < rowCount - 1 && j < colCount - 1 && rows[i][j + 1] == '*' && rows[i + 1][j] == '*') // topLeft
                         {
                             frames.Add(new Frame() { topLeft = new Coordinate(i, j) });
+                            //Debug.WriteLine($"frames.Count => {frames.Count}");
                         }
-                        if (i < rowCount - 1 && j != 0 && rectangle[i, j - 1] == '*' && rectangle[i + 1, j] == '*') // topRight
+                        if (i < rowCount - 1 && j != 0 && rows[i][j - 1] == '*' && rows[i + 1][j] == '*') // topRight
                         {
                             var farame = frames.Last();
                             int index = frames.IndexOf(farame);
                             farame.topRight = new Coordinate(i, j);
                             frames[index] = farame;
                         }
-                        if (i != 0 && j < colCount - 1 && rectangle[i - 1, j] == '*' && rectangle[i, j + 1] == '*') // bottomLeft
+                        if (i != 0 && j < colCount - 1 && rows[i - 1][j] == '*' && rows[i][j + 1] == '*') // bottomLeft
                         {
                             Coordinate bottomLeft = new(i, j);
-                            var farame = frames.First(f => f.bottomLeft == null && f.bottomRight == null && f.topLeft!.Value.col == bottomLeft.col);
+                            var farame = frames.Single(f => f.bottomLeft == null && f.bottomRight == null && f.topLeft!.Value.col == bottomLeft.col);
                             int index = frames.IndexOf(farame);
                             farame.bottomLeft = bottomLeft;
                             frames[index] = farame;
                         }
-                        if (i != 0 && j != 0 && rectangle[i - 1, j] == '*' && rectangle[i, j - 1] == '*') // bottomRight
+                        if (i != 0 && j != 0 && rows[i - 1][j] == '*' && rows[i][j - 1] == '*') // bottomRight
                         {
                             Coordinate bottomRight = new(i, j);
-                            var farame = frames.First(f => f.bottomRight == null && f.topRight!.Value.col == bottomRight.col && f.bottomLeft!.Value.row == bottomRight.row);
+                            var farame = frames.Single(f => f.bottomRight == null && f.topRight!.Value.col == bottomRight.col && f.bottomLeft!.Value.row == bottomRight.row);
                             int index = frames.IndexOf(farame);
                             farame.bottomRight = bottomRight;
                             frames[index] = farame;
                         }
-
                     }
                 }
             }
